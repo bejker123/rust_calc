12cd88416028023436efa880e7711750d2337381
bejker123 <1piotrkucharski@gmail.com>
2024-02-21 18:31:25 +0100

    docs: remove comment

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,6 @@ mod term;
 mod tokenizer;
 
 fn main() {
-    // println!("{}", dbg_tokenize("1+2/2"));
-    // println!("{:?}", tokenize("1+2/2").parse());
-    // println!("{}", dbg_tokenize("5/5^2"));
-    // println!("{:?}", tokenize("5/5^2").parse());
-    // println!(
-    //     "{}",
-    //     Op::Mul(Box::new(Op::Number(123.0)), Box::new(Op::Number(123.0)))
-    //         .apply()
-    //         .expect_err("")
-    // );
-
     let mut term = Term::new();
     // let mut new_line = true;
     term_write!(term, ">").unwrap();
